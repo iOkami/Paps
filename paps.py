03b148366cc761fba245cd51634b914199a34597
iOkami <49694708+iOkami@users.noreply.github.com>
2024-08-30 12:03:35 -0300

    Update paps.py

--- paps.py
+++ paps.py
@@ -7,7 +7,7 @@ import hashlib
 from requests.packages.urllib3.exceptions import InsecureRequestWarning
 requests.packages.urllib3.disable_warnings(InsecureRequestWarning)
 USE_BASIC_AUTH = 1
-url = "https://192.168.10.198"
+url = "http://192.168.10.198"
 username = "hit"
 password = "Hit#1234"
 # if USE_BASIC_AUTH:
