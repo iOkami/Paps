0e6291636793dc52ce8211f940fdc70479cfcfe9
iOkami <49694708+iOkami@users.noreply.github.com>
2024-08-30 12:02:00 -0300

    Update paps.py

--- paps.py
+++ paps.py
@@ -7,26 +7,33 @@ import hashlib
 from requests.packages.urllib3.exceptions import InsecureRequestWarning
 requests.packages.urllib3.disable_warnings(InsecureRequestWarning)
 USE_BASIC_AUTH = 1
-url = "https://<YourIPAddress>"
-username = "manage"
-password = "Abcd_1234"
-if USE_BASIC_AUTH:
-    # HTTP basic authentication
-    headers = {'datatype':'json'}
-    r = requests.get(url + '/api/login', auth=(username, password), headers=headers, verify=False)
-else:
+url = "https://192.168.10.198"
+username = "hit"
+password = "Hit#1234"
+# if USE_BASIC_AUTH:
+#     # HTTP basic authentication
+#     headers = {'datatype':'json'}
+#     r = requests.get(url + '/api/login', auth=(username, password), headers=headers, verify=False)
+# else:
     # SHA-256 authentication
-    auth_bytes = bytes(username + '_' + password, 'utf-8')
-    auth_string = hashlib.sha256(auth_bytes).hexdigest()
-    headers = {'datatype':'json'}
-    r = requests.get(url + '/api/login/' + auth_string, headers=headers, verify=False )
-    # Extract session key from response
-    response = json.loads(r.content.decode('utf-8'))
-    sessionKey = response['status'][0]['response']
-    # Obtain the health of the system
-    headers = {'sessionKey': sessionKey, 'datatype':'json'}
-    r = requests.get(url+'/api/show/system', headers=headers, verify=False)
+auth_bytes = bytes(username + '_' + password, 'utf-8')
+auth_string = hashlib.sha256(auth_bytes).hexdigest()
+headers = {'datatype':'json'}
+r = requests.get(url + '/api/login/' + auth_string, headers=headers, verify=False )
+print(r)
+print(r.text)
+# Extract session key from response
+response = json.loads(r.content.decode('utf-8'))
+sessionKey = response['status'][0]['response']
 
-print(r.content.decode('utf-8'))
-response = json.loads(r.content)
-print("Health = " + response['system'][0]['health'])
+print(sessionKey)
+# Obtain the health of the system
+headers = {'sessionKey': sessionKey, 'datatype':'json'}
+r = requests.get(url+'/api/show/system', headers=headers, verify=False)
+
+print(r)
+print(r.text)
+
+# print(r.content.decode('utf-8'))
+# response = json.loads(r.content)
+# print("Health = " + response['system'][0]['health'])
