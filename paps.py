414fcf70b5d42408ea3bc0624ff436826425f760
iOkami <49694708+iOkami@users.noreply.github.com>
2024-08-30 12:12:24 -0300

    Update paps.py

--- paps.py
+++ paps.py
@@ -8,8 +8,8 @@ from requests.packages.urllib3.exceptions import InsecureRequestWarning
 requests.packages.urllib3.disable_warnings(InsecureRequestWarning)
 USE_BASIC_AUTH = 1
 url = "https://192.168.10.198"
-username = "manage"
-password = "!manage"
+username = "hit"
+password = "Hit234"
 # if USE_BASIC_AUTH:
 #     # HTTP basic authentication
 headers = {'datatype':'json'}
